fe98b7df3e3611bc79386839512a98062b073534
gonchiman <legendaryyamasama@gmail.com>
2026-06-01 21:35:00 +0900

    ref

--- test_get_start_month.py
+++ test_get_start_month.py
@@ -2,4 +2,5 @@ from get_start_month import get_start_month
 from constants import GameTitle
 
 
-print(get_start_month(GameTitle.STREET_FIGHTER_6.value))
+for game_title in GameTitle:
+    print(f"{game_title.value} {get_start_month(game_title.value)}")
